0c7e01c752d5111774e2530994c6de941e72ac68
Lukáš Matuška <lukynmatuska@gmail.com>
2018-11-17 12:21:26 +0100

    Add missing file dbCredentials.php

--- setup/dbCredentials.php
+++ setup/dbCredentials.php
@@ -1,7 +1,8 @@
 <?php 
-// Here we must enter variables for connection to database
-$servername = "buchticka.eu";
-$username = "carodky";
-$password= "VKGSXcKmO7vpZL5O";
-$dbname = "quiz"; // We recommend name `quiz`, but you can try others
+    // Here we must enter variables for connection to database
+    $servername = "localhost";
+    $username = "root";
+    $password= "";
+    $dbname = "quiz";
+
 ?>
