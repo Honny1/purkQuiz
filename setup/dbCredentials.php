ea6e099eceb6cb74f49fe5cbe20535c6a4ab2f54
Hony Rodák <hony.com@seznam.cz>
2018-11-18 20:21:49 +0100

    fix gitignore

--- setup/dbCredentials.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    // Here we must enter variables for connection to database
-    $servername = "localhost";
-    $username = "carodky";
-    $password= "VKGSXcKmO7vpZL5O";
-    $dbname = "quiz";
-
-?>
